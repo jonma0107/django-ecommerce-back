a3616be4eecacb88893cae2c8ac6893ed7ed166e
jonma0107 <jduplantier44@gmail.com>
2023-08-07 12:43:49 -0500

    [CREATE] new url for APIView ProductDetail

--- tusmotos_app/urls.py
+++ tusmotos_app/urls.py
@@ -4,4 +4,5 @@ from tusmotos_app import views
 
 urlpatterns = [
   path('products-list/', views.ProductsList.as_view()),
+  path('products/<slug:category_slug>/<slug:product_slug>/', views.ProductDetail.as_view()),
 ]
